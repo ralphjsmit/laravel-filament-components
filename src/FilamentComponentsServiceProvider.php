d701f36346f10825fd11c7c15e65b13815e2d5f2
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-01-29 13:32:14 +0100

    Configure package

---
+++ src/FilamentComponentsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace RalphJSmit\Filament\Components;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class FilamentComponentsServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('laravel-filament-components')
+            ->hasConfigFile();
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,26 +1,22 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace RalphJSmit\Filament\Components\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use VendorName\Skeleton\SkeletonServiceProvider;
+use RalphJSmit\Filament\Components\FilamentComponentsServiceProvider;
 
 class TestCase extends Orchestra
 {
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            SkeletonServiceProvider::class,
+            FilamentComponentsServiceProvider::class,
         ];
     }
 
@@ -29,7 +25,7 @@ class TestCase extends Orchestra
         config()->set('database.default', 'testing');
 
         /*
-        $migration = include __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
+        $migration = include __DIR__.'/../database/migrations/create_laravel-filament-components_table.php.stub';
         $migration->up();
         */
     }
