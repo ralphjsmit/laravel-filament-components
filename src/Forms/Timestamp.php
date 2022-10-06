0c0aa4717dce414ecdf38ec20c2809edb2f562e9
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-10-06 20:30:12 +0200

    Limit to available translations

--- src/Forms/Timestamp.php
+++ src/Forms/Timestamp.php
@@ -9,7 +9,18 @@ class Timestamp
     public static function make(string $column, string $label = null): Placeholder
     {
         return Placeholder::make($column)
-            ->label($label ?? tr("time.$column"))
+            ->label(function () use ($column, $label) {
+                if ($label) {
+                    return $label;
+                }
+
+                return match ( $column ) {
+                    'created_at' => tr('time.created_at'),
+                    'updated_at' => tr('time.updated_at'),
+                    'deleted_at' => tr('time.deleted_at'),
+                    default => null,
+                };
+            })
             ->content(fn ($record): string => $record?->$column ? $record->$column->diffForHumans() : '-');
     }
 }
