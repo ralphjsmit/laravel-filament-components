2915aebdc9f63787486d6daaa4d3cf22a88baade
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-11-10 17:54:41 +0100

    Fix  Unable to locate a class or view for component [filament-support::grid]

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace RalphJSmit\Filament\Components\Tests;
 
 use Filament\Forms\FormsServiceProvider;
+use Filament\Support\SupportServiceProvider;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RalphJSmit\Filament\Components\FilamentComponentsServiceProvider;
@@ -20,6 +21,7 @@ class TestCase extends Orchestra
             FilamentComponentsServiceProvider::class,
             LivewireServiceProvider::class,
             FormsServiceProvider::class,
+            SupportServiceProvider::class,
         ];
     }
 
