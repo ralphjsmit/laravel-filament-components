dd29021d7272104de41c2efc6eef79a832321c95
Sam Maosa <coolsam726@users.noreply.github.com>
2023-07-27 21:15:06 +0300

    Update Sidebar's Form import path for compatibility with Filament 3 (#20)
    * Update Sidebar.php
    
    Extended parameter and return types for Compatibility with Filament 3 which now uses \Filament\Forms\Form instead of \Filament\Resources\Form
    
    * WIP
    
    * Prefer V3 import as default
    
    ---------
    
    Co-authored-by: Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>

--- src/Forms/Sidebar.php
+++ src/Forms/Sidebar.php
@@ -3,24 +3,28 @@
 namespace RalphJSmit\Filament\Components\Forms;
 
 use Filament\Forms\Components\Grid;
-use Filament\Resources\Form;
+use Filament\Forms\Form;
+use Filament\Resources\Form as FormV2;
 
 class Sidebar
 {
     public function __construct(
-        public Form $form,
+        public FormV2|Form $form,
     ) {}
 
-    public static function make(Form $form = null): static
+    public static function make(FormV2|Form|null $form = null): static
     {
         if ( ! $form ) {
-            $form = Form::make();
+            $form = match ( true ) {
+                class_exists(FormV2::class) => FormV2::make(),
+                default => Form::make(),
+            };
         }
 
         return new static(form: $form);
     }
 
-    public function schema(array $mainComponents, array $sidebarComponents): Form
+    public function schema(array $mainComponents, array $sidebarComponents): FormV2|Form
     {
         return $this->form->schema([
             Grid::make(['sm' => 3])->schema([
