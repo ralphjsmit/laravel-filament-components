7c4a1f1f115c0243b7eabc6fdbce3f898458c738
Alex Manase <malexandru699@gmail.com>
2023-09-11 12:47:53 +0300

    fix `Sidebar`

--- src/Forms/Sidebar.php
+++ src/Forms/Sidebar.php
@@ -2,35 +2,21 @@
 
 namespace RalphJSmit\Filament\Components\Forms;
 
+use Closure;
 use Filament\Forms\Components\Grid;
-use Filament\Forms\Form;
-use Filament\Resources\Form as FormV2;
 
 class Sidebar
 {
     public function __construct(
-        public FormV2|Form $form,
+        public array|Closure $mainComponents,
+        public array|Closure $sidebarComponents
     ) {}
 
-    public static function make(FormV2|Form|null $form = null): static
+    public static function make(array|Closure $mainComponents, array|Closure $sidebarComponents): Grid
     {
-        if ( ! $form ) {
-            $form = match ( true ) {
-                class_exists(FormV2::class) => FormV2::make(),
-                default => Form::make(),
-            };
-        }
-
-        return new static(form: $form);
-    }
-
-    public function schema(array $mainComponents, array $sidebarComponents): FormV2|Form
-    {
-        return $this->form->schema([
-            Grid::make(['sm' => 3])->schema([
+        return Grid::make(['sm' => 3])->schema([
                 Grid::make()->schema($mainComponents)->columnSpan(['sm' => 2]),
                 Grid::make()->schema($sidebarComponents)->columnSpan(['sm' => 1]),
-            ]),
-        ]);
+            ]);
     }
 }
